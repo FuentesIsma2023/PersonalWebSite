4b24316f5d09691b1e581b4e4f08af7cbf86064c
Ismael Najera <128210395+FuentesIsma2023@users.noreply.github.com>
2025-10-18 16:30:48 -0600

    Dates Updated

--- src/components/Learning.tsx
+++ src/components/Learning.tsx
@@ -5,56 +5,56 @@ export default function Learning() {
     {
       id: 1,
       title: 'Cloud Computing AWS, Azure, Google Cloud',
-      date: '2025-10-15',
+      date: 'Current',
       excerpt: 'Started exploring Google Cloud, AWS, and Azure through hands-on labs. Working through the fundamentals of compute, storage, and networking. The practical approach helps solidify concepts that seemed abstract in theory.',
       category: 'Cloud Computing'
     },
     {
       id: 2,
       title: 'Scripting with Python',
-      date: '2025-10-08',
+      date: '2025-09-08',
       excerpt: 'Python script that organizes my downloads folder by file extension',
       category: 'Scripting'
     },
     {
       id: 3,
       title: 'Scripting with Bash',
-      date: '2025-06-30',
+      date: '2025-09-16',
       excerpt: 'Created my first collection of bash scripts to handle daily tasks. My main project is an Automator-activated script for Mac that automates the initial steps of drafting emails in Outlook. It prompts for the email type (start, follow-up, or ticket closing), uses a specific template, copies the text, and opens Outlook, ready for pasting.',
       category: 'Scripting',
     },
     {
       id: 4,
       title: 'Scripting with PowerShell',
-      date: '2025-09-22',
+      date: '2025-09-01',
       excerpt: 'Developed several utilities to streamline daily tasks. Projects include a script to quickly launch my daily applications, one to retrieve Access Control Lists (ACLs) for file systems, and another to list all currently mapped shared drives.',
       category: 'Scripting'
     },
     {
       id: 5,
       title: 'First Steps with Kubernetes',
-      date: '2025-09-22',
+      date: '2025-07-22',
       excerpt: 'Completed initial labs, focusing on the basics of orchestration. Deployed a simple web application using a Deployment, exposed it with a Service, and gained a fundamental understanding of Pods, Nodes, and kubectl commands.',
       category: 'DevOps'
     },
     {
       id: 6,
       title: 'First Steps with Docker',
-      date: '2025-09-22',
+      date: '2025-06-22',
       excerpt: 'Took my first steps with containerization. I successfully built a custom Docker image for a simple application, pushed it to a public registry, and ran it locally, learning the core concepts of Dockerfiles and image layers.',
       category: 'DevOps'
     },
     {
       id: 7,
       title: 'First Steps with Terraform',
-      date: '2025-09-22',
+      date: '2025-05-22',
       excerpt: 'Began exploring Infrastructure as Code. Completed my first project by writing a basic extt.tf file to provision a single virtual machine (VM) and a virtual network in a cloud provider, learning the fundamental workflow of init, plan, and apply',
       category: 'DevOps'
     },
     {
       id: 8,
       title: 'Cloud Networking',
-      date: '2025-05-25',
+      date: '2025-03-25 - Current',
       excerpt: 'Began exploring Infrastructure as Code. Completed my first project by writing a basic extt.tf file to provision a single virtual machine (VM) and a virtual network in a cloud provider, learning the fundamental workflow of init, plan, and apply',
       category: 'Networking, Cloud Computing'
     }
